e6e1436e9213f53b48d0b4c92167584bd1ab4a8d
shahparth36 <parthshah1936@gmail.com>
2021-03-23 17:16:29 +0530

    Added location of a campground feature

--- v16/app.js
+++ v16/app.js
@@ -9,49 +9,52 @@ var User = require("./models/user");
 // var seedDB = require("./seeds");
 var flash = require("connect-flash");
 
-
 //REQURING ROUTES
 var campgroundRoutes = require("./routes/campgrounds");
 var commentsRoutes = require("./routes/comments");
 var authRoutes = require("./routes/auth");
 
-
-var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v15Deployed"
+var url =
+  process.env.DATABASEURL ||
+  "mongodb://localhost/yelp_camp_v15Deployed-withmap";
 
 mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
 // seedDB();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 //-------------------------------------------------------
 
 //PASSPORT CONFIGURATION
-app.use(require("express-session")({
-	secret: "Monty is so single!",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    secret: "Monty is so single!",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-app.use(function(req, res, next) {
-	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
 
-	next();
+  next();
 });
 app.use(authRoutes);
 app.use(campgroundRoutes);
 app.use(commentsRoutes);
 
-app.listen(process.env.PORT || 3000, process.env.IP, function() {
-	console.log("YelpCamp serving on PORT 3000!");
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
+  console.log("YelpCamp serving on PORT 3000!");
 });
--- v16/models/campground.js
+++ v16/models/campground.js
@@ -3,24 +3,27 @@ var mongoose = require("mongoose");
 // Schema Setup
 
 var campgroundSchema = new mongoose.Schema({
-	name: String,
-	price: String,
-	image: String,
-	imageId: String,
-	Description: String,
-	createdAt: { type: Date, default: Date.now },
-	author: {
-		id: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User"
-		},
-		username: String
-	},
-	comments: [{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Comment"
-	}]
+  name: String,
+  price: String,
+  image: String,
+  imageId: String,
+  Description: String,
+  coordinates: Array,
+  Location: String,
+  createdAt: { type: Date, default: Date.now },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    username: String,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
 });
-
 
 module.exports = mongoose.model("Campground", campgroundSchema);
